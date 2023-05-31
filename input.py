6f2af88ac0b12ab953331c74c32af13777c39716
2003solomon <solochidera10@gmail.com>
2023-05-31 23:43:16 +0100

    Asking the user to input their name and printing it out

---
+++ input.py
@@ -0,0 +1,13 @@
+# assign the return value of the first input to the variable name
+name = input("what is your name? ")
+
+# print out "hello using comma with the content of the variable name"
+# print("hello,", name)
+"""
+we can also use another way to display my name
+
+print("hello,"+ name) // (+) will not add a space before the variable
+
+"""
+#print out Hi with the content of the varable name using the (+) symbol
+print("Hi," + name)
