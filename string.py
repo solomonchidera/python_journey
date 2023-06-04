43e53aee525a4cf1a61b75385ad37b85666a298f
SolomonChidera <solochidera10@gmail.com>
2023-06-04 18:28:48 +0100

    Learning strings in python

---
+++ string.py
@@ -0,0 +1,19 @@
+# ways of declaring string in python
+#single quote way => 'your_string_value'
+example0 = 'Solomon is coding.'
+print(example0)
+#my output will be = Solomon is coding.
+
+#double quote way => "your_string_value"
+example01 = "Solomon is coding for real."
+print(example01)
+#my output will be = Solomon is coding for real.
+
+
+#if I want to print a program that has double or single quote in them 
+#(0) I can either escape them by adding back slash in front of where I want to put the quotes or
+#(1) By opening my string with 3 double quote (""") and also ending it with 3 double quote(""")
+#This way my string can take any thing with them 
+example02 = """Solomon is trying to learn python; and he is learning it "well" and hope he becomes 'good' in it"""
+print(example02)
+#my output will be = Solomon is trying to learn python; and he is learning it "well" and hope he becomes 'good' in it
